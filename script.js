53adf83f56798aaa9207908c7877ceba22675653
majack <majackwariaka@gmail.com>
2026-01-21 23:15:33 +0300

    created a dom tree for the game's UI created the logic function

--- script.js
+++ script.js
@@ -1,92 +1,88 @@
-let humanScore = 0;
-let computerScore = 0;
-
-const message = document.querySelector(".message");
-function messageToDisplay(text){
-    message.replaceChildren();
-    const paragraph = document.createElement("p");
-    paragraph.textContent = text;
-    message.appendChild(paragraph);
+const body = document.querySelector("body");
+renderGameUI();
+function renderGameUI(){
+    const container = document.createElement("div");
+    container.classList.add("container");
+    renderTitle(container);
+    renderOptions(container);
+    renderChoices(container);
+    renderScores(container);
+    body.appendChild(container);
+    playGame();
 }
-//function for playing the game
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        const message = (`its a tie you both chose ${humanChoice}
-            you have ${humanScore} point(s) against computer's 
-            ${computerScore} point(s)`);
-            messageToDisplay(message)
-        return;
-    }
-
-    const humanWins =
-        (humanChoice === "rock" && computerChoice === "scissors") ||
-        (humanChoice === "paper" && computerChoice === "rock") ||
-        (humanChoice === "scissors" && computerChoice === "paper");
-
-    if (humanWins) {
-        humanScore++;
-        const message = (`you won this round ${humanChoice} beats ${computerChoice}
-            you have ${humanScore} point(s) against computer's 
-            ${computerScore} point(s)`);
-            messageToDisplay(message);
-    } else {
-        computerScore++;
-        const message = (`you loose ${computerChoice} beats ${humanChoice}
-            you have ${humanScore} point(s) against computer's 
-            ${computerScore} point(s)`);
-            messageToDisplay(message);
-    }
+function renderTitle(container){
+    const gameTitle = document.createElement("div");
+    gameTitle.classList.add("title");
+    const header = document.createElement("h1");
+    header.textContent = "Rock Paper Scissors";
+    gameTitle.appendChild(header);
+    container.appendChild(gameTitle);
 }
-
-/*this function gets the computers choice
-between rock paper or scissors 
-*/
-function getComputerChoice() {
-    let rock ="rock";
-    let paper = "paper";
-    let scissors = "scissors";
-
-    let min = 1;
-    let max = 4;
-    
-    let randomNumber = Math.floor(Math.random() * (+max - +min) + +min);
-    if (randomNumber === 1) {
-        return rock;
-    } else if (randomNumber === 2) {
-        return paper;
-    } else {
-        return scissors;
-    }
-
+function renderOptions(container){
+    const optionsContainer = document.createElement("div")
+    optionsContainer.classList.add("options");
+    const options = ["rock","paper","scissors"];
+    options.forEach((option) => {
+        const button = document.createElement("button");
+        button.id = option;
+        button.textContent = option;
+        optionsContainer.appendChild(button)
+    });
+    container.appendChild(optionsContainer);
 }
-const choices = document.querySelectorAll("button");
-choices.forEach((choice) => {
-    choice.addEventListener("click", () => 
-        playGame(choice.id, getComputerChoice()));
-});
-function playGame(humanChoice,computerChoice){
-    playRound(humanChoice,computerChoice);
-    if (humanScore === 5){
-        displaywinner("human");
-    }else if(computerScore === 5){
-        displaywinner("computer");
+function renderChoices(container){
+    const choicesContainer = document.createElement("div");
+    choicesContainer.classList.add("choicesContainer");
+    choices = ["humanChoice","result","computerChoice"];
+    choices.forEach((choice) => {
+        const card = document.createElement("div");
+        card.classList.add(choice);
+        choicesContainer.appendChild(card);
+    });
+    container.appendChild(choicesContainer);
+}
+function renderScores(container){
+    const scoresContainer = document.createElement("div");
+    scoresContainer.classList.add("scoresContainer");
+    scores = ["humanScore","computerScore"];
+    scores.forEach((score) => {
+        const card = document.createElement("div");
+        card.id = score;
+        scoresContainer.appendChild(card);
+    });
+    container.appendChild(scoresContainer);
+}
+function getComputerChoice(){
+    const rock = "rock";
+    const paper = "paper";
+    const scissors = "scissors";
+
+    const randomNum = Math.floor(Math.random() * 3) + 1;
+    if (randomNum === 1){
+        return(rock);
+    }else if(randomNum === 2){
+        return(paper);
+    }else{
+        return(scissors);
     }
 }
-function displaywinner(winner){
-    let winnermessage = document.createElement("h1");
-    let playGround = document.querySelector("#playerSelection");
-    if (winner === "human"){
-        winnermessage.textContent = "YOU WIN!!";
-        humanScore = 0;
-        computerScore = 0;
-        playGround.replaceChildren();
-        playGround.appendChild(winnermessage);
-    } else if(winner === "computer"){
-        winnermessage.textContent = "COMPUTER WINS";
-        computerScore = 0;
-        humanScore = 0;
-        playGround.replaceChildren();
-        playGround.appendChild(winnermessage);
-        
+function getwinner(humanChoice,computerChoice){
+    if (humanChoice === computerChoice){
+        return("tie");
     }
+    humanWins = (humanChoice === "rock" && computerChoice === "scissors") ||
+                (humanChoice === "paper" && computerChoice === "rock") ||
+                (humanChoice === "scissors" && computerChoice === "paper")
+    return humanWins ? "human" : "computer";
+}
+function playGame(){
+    buttons = document.querySelectorAll("button");
+    buttons.forEach((button) => {
+        button.addEventListener("click", (event) => {
+            computerChoice = getComputerChoice()
+            console.log(computerChoice);
+            const winner = getwinner(event.target.id, computerChoice);
+            console.log(winner);
+        });
+    });
 }
